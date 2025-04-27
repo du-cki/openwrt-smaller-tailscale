c7ddf05fb2d2b7cfbd5484ac97036e0033601f43
du-cki <70286349+du-cki@users.noreply.github.com>
2025-04-27 11:28:22 +0400

    fix a bunch of things

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ ARCH=$2
 OS="linux"
 
 if [ -z "$VERSION" ] || [ -z "$ARCH" ]; then
-  echo "Usage: $0 <version> <arch> [goarm]"
+  echo "Usage: $0 <version> <arch>"
   exit 1
 fi
 
@@ -32,10 +32,14 @@ git -c transfer.progress=0 -c advice.detachedHead=false \
   https://github.com/tailscale/tailscale \
   "$WORKDIR/tailscale"
 
-BINARY="tailscale_${VERSION}_${ARCH}${GOARM:+_$GOARM}.combined"
+BASE_NAME="tailscale_${VERSION}_${ARCH}${GOARM:+_$GOARM}"
+
+BINARY="$BASE_NAME.combined"
 echo "→ Building for $OS/$ARCH${GOARM:+ (GOARM=$GOARM)} ($BINARY)"
 
-GOOS=$OS GOARCH=$ARCH GOARM=$GOARM go build -C "$WORKDIR/tailscale" -o "$WORKDIR/$BINARY" -tags ts_include_cli -ldflags="-s -w" ./cmd/tailscaled >/dev/null
+GOOS=$OS GOARCH=$ARCH GOARM=$GOARM go build \
+  -C "$WORKDIR/tailscale" -o "$WORKDIR/$BINARY" -tags ts_include_cli -ldflags="-s -w" \
+  ./cmd/tailscaled >/dev/null
 
 SIZE=$(du -h "$WORKDIR/$BINARY" | awk '{print $1}')
 echo "✓ Built: $BINARY ($SIZE)"
@@ -50,7 +54,7 @@ cp -r ./fs "$WORKDIR/fs"
 
 mv "$WORKDIR/$BINARY" "$WORKDIR/fs/usr/bin/tailscale.combined"
 
-FINAL="tailscale_${VERSION}_${ARCH}${GOARM:+_$GOARM}.tar.gz"
+FINAL="$BASE_NAME.tar.gz"
 tar -czf "./$FINAL" -C "$WORKDIR/fs" .
 
 echo "✓ Successfully built $FINAL"
