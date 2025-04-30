38a94fb7cbb0cd92eacba08f55f953024db8e2e2
du-cki <70286349+du-cki@users.noreply.github.com>
2025-04-30 23:34:59 +0530

    attach version info, fix mipsle builds

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-set -eo pipefail
+#!/bin/bash
+
+set -e
 
 VERSION=$1
 ARCH=$2
@@ -6,42 +8,51 @@ ARCH=$2
 OS="linux"
 
 if [ -z "$VERSION" ] || [ -z "$ARCH" ]; then
-  echo "Usage: $0 <version> <arch>"
+  echo "usage: $0 <version> <arch>"
   exit 1
 fi
 
+if [ "$VERSION" = "lts" ]; then
+  VERSION=$(curl -s https://api.github.com/repos/tailscale/tailscale/releases/latest | jq -r .tag_name | cut -c2-)
+fi
+
 case "$ARCH" in
-  armv6)
-    ARCH="arm"
-    GOARM="6"
-    ;;
-  armv7)
-    ARCH="arm"
-    GOARM="7"
-    ;;
-  mips | mipsle)
-    GOMIPS="softfloat"
-    ;;
-  *)
-    GOMIPS=""
-    GOARM=""
-    ;;
+armv6)
+  ARCH="arm"
+  GOARM="6"
+  ;;
+armv7)
+  ARCH="arm"
+  GOARM="7"
+  ;;
+mips | mipsle)
+  GOMIPS="softfloat"
+  ;;
+*)
+  GOMIPS=""
+  GOARM=""
+  ;;
 esac
 
 WORKDIR=$(mktemp -d)
 echo "→ Cloning into $WORKDIR"
 
-git -c transfer.progress=0 -c advice.detachedHead=false \
-  clone --quiet --filter=blob:none --depth=1 --branch "v${VERSION}" \
+git -c transfer.progress=0 \
+  -c advice.detachedHead=false \
+  clone \
+  --quiet \
+  --filter=blob:none \
+  --depth=1 \
+  --branch "v${VERSION}" \
   https://github.com/tailscale/tailscale \
   "$WORKDIR/tailscale"
 
 BASE_NAME="tailscale_${VERSION}_${ARCH}${GOARM:+_$GOARM}"
 
 BINARY="$BASE_NAME.combined"
-echo "→ Building for $OS/$ARCH${GOARM:+ (GOARM=$GOARM)}${GOMIPS:+ (GOMIPS="$GOMIPS")} ($BINARY)"
 
 env_vars=(
+  CGO_ENABLED=0
   GOOS="$OS"
   GOARCH="$ARCH"
 )
@@ -49,24 +60,37 @@ env_vars=(
 [ -n "$GOMIPS" ] && env_vars+=(GOMIPS="$GOMIPS")
 [ -n "$GOARM" ] && env_vars+=(GOARM="$GOARM")
 
+echo "→ Generating version info for $OS/$ARCH${GOARM:+ (GOARM=$GOARM)}${GOMIPS:+ (GOMIPS="$GOMIPS")}"
+
+eval $(
+  go run \
+    -C "$WORKDIR/tailscale" \
+    ./cmd/mkversion
+)
+
+ldflags="-X tailscale.com/version.longStamp=${VERSION_LONG} \
+  -X tailscale.com/version.shortStamp=${VERSION_SHORT} -s -w -extldflags=-static"
+
+SHORT_COMMIT_HASH=$(echo $VERSION_GIT_HASH | cut -c1-7)
+echo "→ Building v$VERSION_SHORT (@$SHORT_COMMIT_HASH) on $VERSION_TRACK"
+
 env "${env_vars[@]}" \
   go build \
-    -C "$WORKDIR/tailscale" \
-    -o "$WORKDIR/$BINARY" \
-    -tags netgo,ts_include_cli \
-    -ldflags="-s -w -extldflags=-static" \
-    -trimpath \
-    -buildvcs=false \
-    ./cmd/tailscaled >/dev/null
+  -C "$WORKDIR/tailscale" \
+  -o "$WORKDIR/$BINARY" \
+  -tags netgo,ts_include_cli \
+  -ldflags="$ldflags" \
+  -trimpath \
+  ./cmd/tailscaled >/dev/null
 
 SIZE=$(du -h "$WORKDIR/$BINARY" | awk '{print $1}')
-echo "✓ Built: $BINARY ($SIZE)"
+echo "✓ Built $BINARY ($SIZE)"
 
 echo "→ Compressing with UPX (--lzma --best)"
 upx --lzma --best "$WORKDIR/$BINARY" >/dev/null
 
 SIZE=$(du -h "$WORKDIR/$BINARY" | awk '{print $1}')
-echo "✓ Compressed: $BINARY ($SIZE)"
+echo "✓ Compressed $BINARY ($SIZE)"
 
 cp -r ./fs "$WORKDIR/fs"
 
